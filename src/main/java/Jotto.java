1b174d8ca819146665b05c68b14023968240901c
vllacuna <vllacuna@csumb.edu>
2026-02-12 00:34:36 -0800

    initial UML addresses

--- src/main/java/Jotto.java
+++ src/main/java/Jotto.java
@@ -1,7 +1,77 @@
+import java.util.ArrayList;
+
 /**
  * @author feng3302
  * @version 0.1.0
  * @Since 1/29/26
  **/
 public class Jotto {
+    private int WORD_SIZE;
+    private String currentWord;
+    private int score;
+    private ArrayList<String> playGuesses;
+    private ArrayList<String> playWords;
+    private String filename;
+    private ArrayList<String> wordList;
+    private boolean DEBUG;
+
+    public Jotto(String str) {
+
+    }
+
+    public boolean pickWord() {
+
+    }
+
+    public String showWordList() {
+
+    }
+
+    public ArrayList<String> showPlayerGuesses() {
+
+    }
+
+    public void playerGuessScores(ArrayList<String> arrayList) {
+
+    }
+
+    public void setCurrentWord(String str) {
+
+    }
+
+    public ArrayList<String> readWords() {
+
+    }
+
+    public void play() {
+
+    }
+
+    public int guess() {
+
+    }
+
+    public ArrayList<String> getPlayedWords() {
+
+    }
+
+    public String getCurrentWord() {
+
+    }
+
+    public int getLetterCount(String str) {
+
+    }
+
+    public String showPlayedWords() {
+
+    }
+
+    public boolean addPlayerGuess(String str) {
+
+    }
+
+    public void updateWordList() {
+
+    }
 }
